be4c5a0578014fcf91e94e2029a00695a3d598a0
cudoh <cudoh@student.42wolfsburg.de>
2026-04-13 05:40:14 +0200

    first commit

---
+++ 01_tut/js/change_status.js
@@ -0,0 +1,11 @@
+var inStock;
+var outStock;
+
+inStock = true;
+outStock = false;
+
+var elOrange = document.getElementById('idOrange');
+elOrange.className = 'outstock';
+
+var elPear = document.getElementById('idPear');
+elPear.className = 'instock';
